05305256d03ebccd617117f0420812a3f16ba393
bilalashraf1710 <bilalashraf1710@gmail.com>
2021-06-04 19:24:33 +0500

    enable cors

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const product = require("./routes/products");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const app = express();
+var cors = require('cors')
+
 
 mongoose
   .connect("mongodb+srv://bilalash:salesforce@123@cluster0.dghlp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
@@ -13,6 +15,7 @@ mongoose
   .then(() => console.log("connect to mongodb"))
   .catch((err) => console.log("could not connect to mongodb"));
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/products", product);
 app.use("/api/users", users);
